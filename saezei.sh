0412074874f3f65c68e2adfc0bd3bc8ec88a5c9d
Karsten Heinze <karsten.heinze@sidenotes.de>
2016-05-31 23:07:31 +0200

    Added shell script wrapper.

---
+++ saezei.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+BASEDIR=$HOME/backup/sz
+
+OS=$(uname -s)
+
+if [[ "$OS" == "OpenBSD" ]]; then
+    ps -a | awk '$5 ~ /Xvfb/ { print $5; }' | grep -q Xvfb
+else
+    ps -a | awk '$4 ~ /Xvfb/ { print $4; }' | grep -q Xvfb
+fi
+
+if (( $? != 0 )); then
+    nohup Xvfb :1 -screen 1 1600x1200x8 &>/dev/null &
+    sleep 1
+fi
+
+DISPLAY=:1 saezei.rb $BASEDIR 2>> $BASEDIR/saezei.log
+if (( $? != 0 )); then
+    exit 1
+fi 
+
+DIR=$(ls -tr $BASEDIR/*/ | tail -1)
+
+saezei_convert.rb $BASEDIR/$DIR 2>> $BASEDIR/saezei_convert.log
+if (( $? != 0 )); then
+    exit 2
+fi 
+
+saezei_compile.rb $BASEDIR/$DIR 2>> $BASEDIR/saezei_compile.log
+if (( $? != 0 )); then
+    exit 2
+fi 
